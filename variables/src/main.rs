82b52231e7ebbe33285f4f5002a1ea8b146fefab
Kishan2595 <kishan95.kumar@gmail.com>
2020-07-24 20:12:57 +0530

    var added.

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 13;
+
+    println!("Value of x is {}", x);
+
+    x = 26; //since x is already declared as var, we did not need to mention "let" again.
+
+    println!("Value of x is now {}", x);
+}
+
+
