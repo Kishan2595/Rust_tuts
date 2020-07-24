6df4d2d957a4d6808cf59d47480419a9183a4810
Kishan2595 <kishan95.kumar@gmail.com>
2020-07-24 23:07:15 +0530

    If else added

---
+++ If_Else/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let n = 56;
+
+    if n == 45 {
+        println!("The number is 45.");
+    }
+    else if n > 45 {
+        println!("It is greater than 45.");
+    }
+    else {
+        println!("It was not 45, it was something less than 45.");
+    }
+}
