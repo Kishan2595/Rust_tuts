358062bc34ea2afbce54c5c3e1e7e5acb303f8f0
Kishan2595 <kishan95.kumar@gmail.com>
2020-07-25 23:28:07 +0530

    reference added.

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let mut x = 10;                  // we setup a value for x
+    let y = &x;                      // now we refer it to y and z
+    let z = &x;                      // which means vakue of x can be accessed  through y and z.
+
+    println!("Value of x is {}", x);
+    println!("Value of x is {}", x);
+    println!("Value of x is {}\n", x);
+
+    //y += 1;                        // but changes can't be made through ref var as they are immutable.
+
+    //  ** to change the value through ref **
+
+    let mut a = 20;
+    let b = &mut a;
+    
+    *b += 1;                        // to make it mutable we need to add * as shown
+
+    println!("Value of a is {}", b);
 }
+
