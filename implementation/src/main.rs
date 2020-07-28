f95e0b0be2f4e9c2dc5e371b66ec7493bd596fa6
Kishan2595 <kishan95.kumar@gmail.com>
2020-07-28 20:41:05 +0530

    modified

--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -1,3 +1,24 @@
+struct Rectangle {
+    width: u32,
+    length: u32
+}
+   
+impl Rectangle {
+
+    fn print_description(&self) {
+        println!("Rectangle {} * {}", self.width, self.length)
+    }
+
+    fn is_square(&self) -> bool {
+        self.width == self.length
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let my_rect = Rectangle {width: 10, length: 15};
+
+    my_rect.print_description();
+    
+    println!("Rectangle mentioned here is a square: {}", my_rect.is_square());
 }
